eba6a658220b2f0283cf1ecae1c456200cd1da07
alibaba0010 <zakariyahali100@gmail.com>
2024-02-06 16:59:11 +0100

    working with the backend

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -15,3 +15,53 @@ import {
 import dotenv from "dotenv";
 import UnAuthorizedError from "../errors/unauthorized.js";
 dotenv.config();
+
+// CREATE NEW USER
+export async function httpAddNewUser(req, res) {
+  const { username, email, password, confirmPassword } = req.body;
+
+  comparePassword(password, confirmPassword);
+
+  requiredFields(username, email, password, confirmPassword);
+
+  await checkIfExists(email, username);
+
+  const user = await User.create({ username, email, password });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ username: user.username, email: user.email, id: user._id });
+}
+//USER LOGIN
+export async function httpLogin(req, res) {
+  const { value, password } = req.body;
+  if (!value || !password)
+    throw new BadRequestError("Provide a username or email and password");
+  const user = await checkValue(value);
+  const comparePassword = await user.comparePassword(password);
+  if (!comparePassword) throw new UnAuthenticatedError("Invalid Password");
+  const token = await user.createJWT();
+  req.session = {
+    jwt: token,
+  };
+  res.status(StatusCodes.OK).json({ id: user.id, username: user.username });
+}
+
+// SHOW CURRENT USER
+export const showCurrentUser = async (req, res) => {
+  const { userId } = req.user;
+  const user = await User.findById(userId);
+  if (!user) throw new notFoundError("Unable to get User");
+
+  const { username, id, email, isAdmin } = user;
+
+  return res.status(StatusCodes.OK).json({ username, id, email, isAdmin });
+};
+
+// LOGOUT USER
+export const logOutUser = async (req, res) => {
+  const { userId } = req.user;
+  await findUser(userId);
+
+  req.session = null;
+  return res.status(StatusCodes.OK).json({ msg: "Successfully logged out" });
+};
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import {} from "../controller/user.controller.js";
 import {
-   // authenticateUser,
-   // verifyAdmin,
-   // verifyUser,
- } from "../middleware/auth.js";
-const userRouter = Router()
-userRouter.post("/user")
-export default userRouter
+  httpAddNewUser,
+  httpLogin,
+  logOutUser,
+  showCurrentUser,
+} from "../controller/user.controller.js";
+import {
+  authenticateUser,
+  verifyAdmin,
+  verifyUser,
+} from "../middleware/auth.js";
+const userRouter = Router();
+userRouter
+  .post("/users/register", httpAddNewUser)
+  .post("/users/login", httpLogin)
+  .get("/user", authenticateUser, verifyUser, showCurrentUser)
+  .get("/users/logout", authenticateUser, verifyUser, logOutUser);
+
+export default userRouter;
